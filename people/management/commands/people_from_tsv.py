12f7aa178ff3be8dcdbc07afcb016fa3a18cec3b
Ignisor <Ignis2497@gmail.com>
2022-12-16 18:57:14 +0200

    feat: :sparkles: Birth date not required on import command

--- people/management/commands/people_from_tsv.py
+++ people/management/commands/people_from_tsv.py
@@ -7,29 +7,29 @@ from django.core.management.base import BaseCommand
 
 from people.models import Person, PersonInfoPoint
 
-PHOTOS_FOLDER = Path('photos/')
+PHOTOS_FOLDER = Path("photos/")
 
 
 class Command(BaseCommand):
-    help = 'Imports persons from CSV'
+    help = "Imports persons from CSV"
 
     def add_arguments(self, parser):
-        parser.add_argument('filepath', type=str)
+        parser.add_argument("filepath", type=str)
 
     def handle(self, *args, **options):
-        with open(options['filepath']) as file:
-            reader = DictReader(file, delimiter='\t')
+        with open(options["filepath"]) as file:
+            reader = DictReader(file, delimiter="\t")
 
             for row in reader:
                 person = Person.objects.create(
-                    name=row.pop('name'),
-                    birth_date=datetime.strptime(row.pop('birth_date'), '%d.%m.%Y'),
-                    group=int(row.pop('group')),
+                    name=row.pop("name"),
+                    birth_date=datetime.strptime(row.pop("birth_date"), "%d.%m.%Y") if row.get("birth_date") else None,
+                    group=int(row.pop("group")),
                 )
 
-                photo_path = row.pop('photo', None)
+                photo_path = row.pop("photo", None)
                 if photo_path:
-                    with open(PHOTOS_FOLDER / photo_path, 'rb') as photo:
+                    with open(PHOTOS_FOLDER / photo_path, "rb") as photo:
                         img = ImageFile(photo)
                         person.photo.save(photo_path, img)
 
@@ -44,4 +44,4 @@ class Command(BaseCommand):
                         info=answer,
                     )
 
-                print(f'Created {person}')
+                print(f"Created {person}")
