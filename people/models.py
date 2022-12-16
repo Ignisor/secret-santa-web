00ee6e5c936fb8961866c72097b087924d538481
Ignisor <Ignis2497@gmail.com>
2022-12-16 15:16:43 +0200

    feat: :sparkles: Person's birth date can be Null

--- people/models.py
+++ people/models.py
@@ -8,16 +8,22 @@ from base.models import UUIDModel
 
 class Person(UUIDModel):
     name = models.TextField()
-    photo = models.ImageField(null=True)
-    birth_date = models.DateField()
+    photo = models.ImageField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
     pin = models.CharField(max_length=4, null=True, blank=True)
     group = models.IntegerField(null=True, blank=True)
-    present_to = models.OneToOneField('Person', on_delete=models.SET_NULL, related_name='present_from',
-                                      null=True, blank=True, editable=False)
+    present_to = models.OneToOneField(
+        "Person",
+        on_delete=models.SET_NULL,
+        related_name="present_from",
+        null=True,
+        blank=True,
+        editable=False,
+    )
 
     def __str__(self) -> str:
-        return f'{self.name} ({self.group})'
+        return f"{self.name} ({self.group})"
 
     @property
     def number(self) -> int:
@@ -34,15 +40,15 @@ class Person(UUIDModel):
     @property
     def target_dossier_url(self):
         if self.present_to:
-            return f'{settings.SITE_URL}dossier/{self.present_to.id}/'
-        return '-'
+            return f"{settings.SITE_URL}dossier/{self.present_to.id}/"
+        return "-"
 
     @property
     def birth_year(self):
-        return self.birth_date.year
+        return self.birth_date.year if self.birth_date else None
 
 
 class PersonInfoPoint(UUIDModel):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='info_points')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="info_points")
     heading = models.TextField()
     info = models.TextField()
